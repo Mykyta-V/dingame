58cc8cc9f799eae0d2357af8eb4bbbe07f4e0944
mykyta <nikita.velko7@gmail.com>
2026-02-28 11:16:35 +0100

    Опис змін

--- game.py
+++ game.py
@@ -1,32 +1,34 @@
 import pygame
-
-clock = pygame.time.Clock()
+import sys
 
 pygame.init()
+clock = pygame.time.Clock()
+
 screen = pygame.display.set_mode((900, 300))
 pygame.display.set_caption("Dinosaur Game")
 pygame.display.set_icon(pygame.image.load("images/bones.png"))
-fon = pygame.image.load("images/fon.png")
+fonn = pygame.image.load("images/fonn.png")
 
 bg_x = 0
 
-Dino = pygame.image.load("images/dinosaur.png")
+Dino = pygame.image.load("images/dino.png")
 
 running = True
 while running:
-    screen.blit(fon, (bg_x, 0))
-    screen.blit(fon, (bg_x + 900, 4))
-    screen.blit(Dino, (10, 230))
-    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    screen.blit(fonn, (bg_x, 0))
+    screen.blit(fonn, (bg_x + 900, 0))
+    screen.blit(Dino, (10, 195))
+    pygame.display.update()
 
     bg_x -= 10
     if bg_x <= -900:
         bg_x = 0
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-
     clock.tick(15)
+
+pygame.quit()
+sys.exit()
